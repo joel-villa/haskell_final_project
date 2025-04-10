4f375a0e2b1439681083e4ea383c408e89e42a55
nrunyan <nrunyan@unm.edu>
2025-04-10 00:05:04 -0600

    test with scroll

--- app/Main.hs
+++ app/Main.hs
@@ -13,24 +13,41 @@ import Tick
 --prelude.show.event, very helpful
 
 --worldToPicture' World -> Picture->Picture->Picture 
+
 main :: IO ()
 
 main =do
-  bmp <- loadBMP "resources/LilDude.bmp"
-  floorbmp <- loadBMP "resources/floor.bmp"
+  bmp <- loadBMP "resources/Sheep.bmp"
+  floorbmp <- loadBMP "resources/pinkGrass.bmp"
+  clouds <- loadBMP "resources/patchOfClouds.bmp"
+
+  let worldToPic = \w -> worldToPicture w bmp floorbmp -- TODO make this able to pass all resources
+  
 
   play
     (InWindow "GameEvent" (1000, 1000) (10, 10))   --Display mode
-    cyan    --Background color.
+    ((makeColor 0.75 0.75 1 0.5))    --Background color.
     100  --Number of simulation steps to take for each second of real time.
-    (World(0, (0.5))([(-60,0),(-47,0),(-33,0),(-12,0),(-2,0),(11,0),(27,0),(49,0)]))   --The initial world.
-    (\world -> (pictures (worldToPicture world bmp floorbmp))) --A function to convert the world a picture.
+    (World(0, (-12))(makeTup (-100.0) 100.0 10.5) 0)   --The initial world.
+    (\world -> (pictures (worldToPicture world bmp floorbmp clouds))) --A function to convert the world a picture.
     handleEvent   -- (Event -> world -> world) A function to handle input events.
     tick --(Float -> world -> world)
 
+makeTup :: Float-> Float->Float->[(Float,Float)]
+makeTup start end step
+  |start > end =[((start-(5.0*step)),-20),((-start+(3.0*step)),-20)] 
+  |otherwise =[(start+step,(-30))] ++ makeTup (start+step) end step
+
+
 -- data World = World { pos  :: (Float,Float)   }     -- current level depth}  -- all levels
-worldToPicture :: World -> Picture->Picture-> [Picture]
-worldToPicture (World(x,y) []) pic floorpic= [Translate (20*x) (20*y) (pic)] 
-worldToPicture (World(x,y) ((z,w):fs)) pic floorpic = Translate (5*z) (5*w) (floorpic) : worldToPicture (World(x,y) (fs)) pic floorpic
+worldToPicture :: World -> Picture->Picture->Picture-> [Picture]
+worldToPicture (World(x,y) [] offset) pic floorpic clouds= Translate (20*(x-offset)) (20*y) (pic) :[Scale 2.5 2.5 (Translate 20 80 clouds)]
+worldToPicture (World(x,y) ((z,w):fs) offset) pic floorpic clouds = scale 2 2 (Translate (5*(z-offset)) (5*w) (floorpic)) : worldToPicture (World(x,y) (fs) offset) pic floorpic clouds
 --TODO make this capable of 
+
+{-
+if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or (
+                (player.rect.left - offset_x <= scroll_area_width) and player.x_vel < 0):
+            offset_x += player.x_vel
+-}
 
