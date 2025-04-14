028ae3206ffcf0d94e6fe9d2e8a62094df1c0d63
Joel Villarreal <joel.villa2329@gmail.com>
2025-04-14 11:14:09 -0600

    moved starterSheep to Init file

--- app/Init.hs
+++ app/Init.hs
@@ -1,3 +1,14 @@
 module Init where
 import Brillo
+import Types
 
+-- Your classic begener sheep 
+starterSheep =
+  Player{
+    xPos =0,
+    yPos =(-12),
+    health=100, 
+    fallingTime=0,
+    money=10, 
+    sMoneyAndSValubles=[Potion {quantEffect=10,descriptor="Health Potion +10", effect=Healing}]
+  }
--- app/Types.hs
+++ app/Types.hs
@@ -24,15 +24,6 @@ data BadGuy = BadGuy {health_bad :: Int,
                       xVel_bad:: Float}
 
 
--- Your classic begener sheep 
-starterSheep =Player{xPos =0,
-                     yPos =(-12),
-                     health=100, 
-                     fallingTime=0,
-                     money=10, 
-                     sMoneyAndSValubles=[Potion {quantEffect=10,descriptor="Health Potion +10", effect=Healing}]
-                     }
-
 
 data Item = 
   Potion {
