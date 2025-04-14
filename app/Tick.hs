9f47c9f71ff7dd87509e42da72bfb2490a54a2ab
Joel Villarreal <joel.villa2329@gmail.com>
2025-04-14 12:40:57 -0600

    simple movement logic

--- app/Tick.hs
+++ app/Tick.hs
@@ -2,4 +2,13 @@ module Tick where
 import Types
 
 tick :: Float -> World -> World
-tick _ world = world
+tick _ w = newWorld
+  where 
+    newPlayer = updatePlayer (hero w)
+    newWorld = w {hero = newPlayer}
+
+updatePlayer :: Player -> Player
+updatePlayer p0 = newP
+  where
+    p1 = p0 {xPos = xVel p0 + xPos p0, yPos = yVel p0 + yPos p0}
+    newP = if inAir p1 then p1 {yVel = yVel p1 - 10} else p1 -- if in Air, fall
