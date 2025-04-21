378e8820315dd981349290e13e6c83068328b6af
Joel Villarreal <joel.villa2329@gmail.com>
2025-04-21 14:55:16 -0600

    fixed arrow issue, (wasd) now

--- app/EventHandler.hs
+++ app/EventHandler.hs
@@ -4,12 +4,12 @@ import Brillo.Interface.IO.Interact
 
 
 newHandleEvent :: Event ->World ->World 
-newHandleEvent(EventKey (SpecialKey KeyRight) Down _ _) w =w{hero= ((hero w){xVel=10})}
-newHandleEvent(EventKey (SpecialKey KeyRight) Up _ _) w =w{hero= ((hero w){xVel=0})}
-newHandleEvent(EventKey (SpecialKey KeyLeft) Down _ _) w =w{hero= ((hero w){xVel=(-5)})}
-newHandleEvent(EventKey (SpecialKey KeyLeft) Up _ _) w =w{hero= ((hero w){xVel=(0)})}
-newHandleEvent(EventKey (SpecialKey KeyUp) Down _ _) w =w{hero= (handleJump (hero w))}
-newHandleEvent(EventKey (SpecialKey KeyUp) Up _ _) w =w{hero= ((hero w){yVel=0})}
+newHandleEvent(EventKey (Char 'd') Down _ _) w =w{hero= ((hero w){xVel=10})}
+newHandleEvent(EventKey (Char 'd') Up _ _) w =w{hero= ((hero w){xVel=0})}
+newHandleEvent(EventKey (Char 'a') Down _ _) w =w{hero= ((hero w){xVel=(-5)})}
+newHandleEvent(EventKey (Char 'a') Up _ _) w =w{hero= ((hero w){xVel=(0)})}
+newHandleEvent(EventKey (Char 'w') Down _ _) w =w{hero= (handleJump (hero w))}
+newHandleEvent(EventKey (Char 'w') Up _ _) w =w{hero= ((hero w){yVel=0})}
 newHandleEvent _ w=w
 
 handleJump :: Player ->Player
