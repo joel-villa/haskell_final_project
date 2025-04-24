aec369019f4a6736329a04f5ee7de509ae4f2682
Joel Villarreal <joel.villa2329@gmail.com>
2025-04-24 10:31:42 -0600

    attempt at hitting head on a block logic, still buggy

--- app/Tick.hs
+++ app/Tick.hs
@@ -43,7 +43,7 @@ getOffset :: World -> Float
 getOffset w= 
   if ((xPos (hero w))-(offset w)>= (200) && (xVel (hero w))>0) ||((xPos (hero w))-(offset w)<=(200)&&xVel (hero w)<0) then (offset w) +(xVel (hero w)) else (offset w)
 
-
+--TODO, should offset be in tick, I feel like it is more well suited for drawing, but idk
 horizontalCollision :: Player -> Float->[JBlock] -> Player
 horizontalCollision p _ [] = if not (inAir p) then  p {inAir =True} else p
 horizontalCollision p offs (block:bs) = 
@@ -52,17 +52,37 @@ horizontalCollision p offs (block:bs) =
   -- (-50) is how many pixels off the left side of the block picture doesnt take up
   -- (-22) is how many pixels the right side of the block picture doesnt take up
   --If you change this, it will break collison in its current form
-  -- Joel's Note: 2*y2 + 80, where 80 was found by guess and check to fix a bug
   -- TODO: should we move this pixel math into draw? 
-  if inBetween (xPos p)  (2*x1-50-offs) (2*x2-22-offs) && inBetween (yPos p) ((2)*y1) (2*y2 + 80) --TODO make this not hard-coded :(  x1-25.5), y1), ((x2-18.5)
-  then p {yPos = ((2)*y1+85), inAir = False, yVel = 0}
+  if collision &&  ((yVel p) < 0.25) -- 0.25 b/c 0 resulted in jumping bug
+    -- falling onto a block
+    then p {yPos = ((2)*y1+85), inAir = False, yVel = 0} 
+  else if collision && ((yVel p ) > 0.25) -- 0.25 b/c 0 resulted in jumping bug
+    -- hitting head on block
+    then p {yPos = (y1 - 120), inAir = True, yVel = 0} -- Somewhat arbitrary 120: TODO: fix inBlock to make this more fluid
+  --TODO check both sides of the block as well (rename this function to collision, instead of just horizontalCollision)
   else horizontalCollision p offs bs
     -- (x1, y1) is bottom left corner (min values)
     -- (x2, y2) is top right (max values)
     where 
+      collision = inBlock block (xPos p, yPos p) offs 
       (x1, y2) = topLeft block
-      x2 = x1 + width block
       y1 = y2 - height block
+      
+inBlock :: JBlock -> (Float, Float) -> Float-> Bool
+-- These numbers are NOT choosen arbituarly
+-- the 2* is for the scalar of the block
+-- (-50) is how many pixels off the left side of the block picture doesnt take up
+-- (-22) is how many pixels the right side of the block picture doesnt take up
+--If you change this, it will break collison in its current form
+-- Joel's Note: 2*y2 + 80, where 80 was found by guess and check
+-- TODO: should we move this pixel math into draw? 
+inBlock block (x, y) offs = inBetween x  (2*x1-50-offs) (2*x2-22-offs) && inBetween y ((2)*y1) (2*y2 + 80) --TODO make this not hard-coded :(  x1-25.5), y1), ((x2-18.5)
+  -- (x1, y1) is bottom left corner (min values)
+  -- (x2, y2) is top right (max values)
+  where 
+    (x1, y2) = topLeft block
+    x2 = x1 + width block
+    y1 = y2 - height block
 
 --checks if there is enough lives to bring the player back and if it past the screen height
 handleFall::Player->Player
