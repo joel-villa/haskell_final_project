74d00c37e4999fa08849240cb6224e179ead7a4c
dthompson2003 <dthompson1@unm.edu>
2025-04-24 11:46:15 -0600

    add to first level

--- app/FirstWorld.hs
+++ app/FirstWorld.hs
@@ -18,11 +18,55 @@ firstWorld x y xs ys= [ Block x y, Block (x+(xs)) y,Block (x+(2.0*xs)) y, Block 
                            Block (x+(27.0*xs)) (y),Block (x+(28.0*xs)) (y),Block (x+(28.0*xs)) (y+(ys)),
                            Block (x+(28.0*xs)) (y+(ys*2)), Block (x+(28.0*xs)) (y+(ys*3)),Block (x+(28.0*xs)) (y+(ys*4)),
                            Block (x+(28.0*xs)) (y+(ys*5)),Block (x+(28.0*xs)) (y+(ys*6)),Block (x+(29.0*xs)) (y+(ys*6)),
-                           Block (x+(30.0*xs)) (y+(ys*6)),Block (x+(31.0*xs)) (y+(ys*6)),Block (x+(32.0*xs)) (y+(ys*6))]
+                           Block (x+(30.0*xs)) (y+(ys*6)),Block (x+(31.0*xs)) (y+(ys*6)),Block (x+(32.0*xs)) (y+(ys*6)),
+                           Block (x+(33.0*xs)) (y+(ys*6)),Block (x+(34.0*xs)) (y+(ys*6)),Block (x+(35.0*xs)) (y+(ys*6)),
+                           Block (x+(36.0*xs)) (y+(ys*6)),Block (x+(36.0*xs)) (y+(ys*5)),Block (x+(36.0*xs)) (y+(ys*4)),
+                           Block (x+(36.0*xs)) (y+(ys*3)),Block (x+(36.0*xs)) (y+(ys*2)),Block (x+(36.0*xs)) (y+(ys*2)),
+                           Block (x+(36.0*xs)) (y+(ys)),Block (x+(36.0*xs)) (y),Block (x+(36.0*xs)) (y-(ys)),
+                           Block (x+(36.0*xs)) (y-(ys*2)),Block (x+(36.0*xs)) (y-(ys*3)), Block (x+(39.0*xs)) (y+(ys*7)),
+                           Block (x+(40.0*xs)) (y+(ys*7)),Block (x+(43.0*xs)) (y+(ys*7)),Block (x+(44.0*xs)) (y+(ys*7)),
+                           Block (x+(47.0*xs)) (y+(ys*5)),Block (x+(48.0*xs)) (y+(ys*5)),Block (x+(51.0*xs)) (y+(ys*6)),
+                           Block (x+(52.0*xs)) (y+(ys*6)),Block (x+(55.0*xs)) (y+(ys*7)),Block (x+(58.0*xs)) (y+(ys*7)),
+                           Block (x+(61.0*xs)) (y+(ys*7)),Block (x+(62.0*xs)) (y+(ys*7)),Block (x+(65.0*xs)) (y+(ys*7)),
+                           Block (x+(66.0*xs)) (y+(ys*7)),Block (x+(67.0*xs)) (y+(ys*7)),Block (x+(68.0*xs)) (y+(ys*7)),
+                           Block (x+(69.0*xs)) (y+(ys*7)),Block (x+(72.0*xs)) (y+(ys*7)),Block (x+(73.0*xs)) (y+(ys*7)),
+                           Block (x+(75.0*xs)) (y+(ys*9)),Block (x+(76.0*xs)) (y+(ys*9)),Block (x+(78*xs)) (y+(ys*7)),
+                           Block (x+(79.0*xs)) (y+(ys*7)),Block (x+(81.0*xs)) (y+(ys*5)),Block (x+(82.0*xs)) (y+(ys*5)),
+                           Block (x+(85.0*xs)) (y+(ys*3)),Block (x+(86.0*xs)) (y+(ys*3)),Block (x+(87.0*xs)) (y+(ys*3)),
+                           Block (x+(90.0*xs)) (y+(ys*3)),Block (x+(91.0*xs)) (y+(ys*3)), Block (x+(93.0*xs)) (y),
+                           Block (x+(94.0*xs)) (y),Block (x+(95.0*xs)) (y),Block (x+(96.0*xs)) (y),Block (x+(97.0*xs)) (y),
+                           Block (x+(98.0*xs)) (y),Block (x+(96.0*xs)) (y+(ys*3)),Block (x+(97.0*xs)) (y+(ys*3)),
+                           Block (x+(98.0*xs)) (y+(ys*3)),Block (x+(99.0*xs)) (y),Block (x+(100.0*xs)) (y),Block (x+(101.0*xs)) (y),
+                           Block (x+(102.0*xs)) (y),Block (x+(102.0*xs)) (y-(ys)),Block (x+(102.0*xs)) (y-(2*ys)),
+                           Block (x+(102.0*xs)) (y-(ys*3)),Block (x+(102.0*xs)) (y-(ys*4)),Block (x+(102.0*xs)) (y-(ys*5)),
+                           Block (x+(105.0*xs)) (y+(ys)),Block (x+(106.0*xs)) (y+(ys)),Block (x+(108.0*xs)) (y+(ys*2)),
+                           Block (x+(109.0*xs)) (y+(ys*2)),Block (x+(111.0*xs)) (y+(ys*4)),Block (x+(112.0*xs)) (y+(ys*4)),
+                           Block (x+(114.0*xs)) (y+(ys*6)),Block (x+(115.0*xs)) (y+(ys*6)),Block (x+(114.0*xs)) (y+(ys*2)),
+                           Block (x+(115.0*xs)) (y+(ys*2)),Block (x+(117.0*xs)) (y+(ys*4)),Block (x+(118.0*xs)) (y+(ys*4)),
+                           Block (x+(120.0*xs)) (y+(ys*3)),Block (x+(122.0*xs)) (y+(ys*2)),Block (x+(124.0*xs)) (y+(ys*3)),
+                           Block (x+(126.0*xs)) (y+(ys*4)), Block (x+(128.0*xs)) (y+(ys*4)),Block (x+(129.0*xs)) (y+(ys*4)),
+                           Block (x+(130.0*xs)) (y+(ys*4)),Block (x+(131.0*xs)) (y+(ys*4)),Block (x+(132.0*xs)) (y+(ys*4)),
+                           Block (x+(129.0*xs)) (y+(ys*6)),Block (x+(130.0*xs)) (y+(ys*6)),Block (x+(131.0*xs)) (y+(ys*6)),
+                           Block (x+(134.0*xs)) (y+(ys*4)),Block (x+(135.0*xs)) (y+(ys*4)),Block (x+(137.0*xs)) (y+(ys*2)),
+                           Block (x+(138.0*xs)) (y+(ys*2)),Block (x+(140.0*xs)) (y),Block (x+(141.0*xs)) (y),
+                           Block (x+(142.0*xs)) (y),Block (x+(143.0*xs)) (y),Block (x+(143.0*xs)) (y+(ys)),Block (x+(143.0*xs)) (y+(ys*2)),
+                           Block (x+(144.0*xs)) (y+(ys*2)),Block (x+(145.0*xs)) (y+(ys*2)),Block (x+(146.0*xs)) (y+(ys*2)),
+                           Block (x+(146.0*xs)) (y+(ys*2)),Block (x+(146.0*xs)) (y+(ys*3)),Block (x+(146.0*xs)) (y+(ys*4)),
+                           Block (x+(147.0*xs)) (y+(ys*4)),Block (x+(148.0*xs)) (y+(ys*4)),Block (x+(149.0*xs)) (y+(ys*4)),
+                           Block (x+(150.0*xs)) (y+(ys*4)),Block (x+(150.0*xs)) (y+(ys*5)),Block (x+(150.0*xs)) (y+(ys*6)),
+                           Block (x+(151.0*xs)) (y+(ys*6)),Block (x+(152.0*xs)) (y+(ys*6)),Block (x+(153.0*xs)) (y+(ys*6)),
+                           Block (x+(154.0*xs)) (y+(ys*6)),Block (x+(155.0*xs)) (y+(ys*6)),Block (x+(155.0*xs)) (y+(ys*5)),
+                           Block (x+(155.0*xs)) (y+(ys*4)),Block (x+(156.0*xs)) (y+(ys*4)),Block (x+(157.0*xs)) (y+(ys*4)),
+                           Block (x+(158.0*xs)) (y+(ys*4)),Block (x+(158.0*xs)) (y+(ys*3)),Block (x+(158.0*xs)) (y+(ys*2)),
+                           Block (x+(158.0*xs)) (y+(ys*1)),Block (x+(158.0*xs)) (y),Block (x+(159.0*xs)) (y),
+                           Block (x+(160.0*xs)) (y),Block (x+(161.0*xs)) (y),Block (x+(161.0*xs)) (y),
+                           Block (x+(162.0*xs)) (y),Block (x+(163.0*xs)) (y),Block (x+(164.0*xs)) (y),
+                           Block (x+(165.0*xs)) (y),Block (x+(166.0*xs)) (y),Block (x+(167.0*xs)) (y),
+                           Block (x+(168.0*xs)) (y),Block (x+(169.0*xs)) (y),Block (x+(170.0*xs)) (y)]
 
 firstWorldToLevelBlock :: [Terrain] -> [JBlock]
 firstWorldToLevelBlock [] = []
-firstWorldToLevelBlock (Block x y : xs) =  reverse((JBlock (x, y) 5 20 None): firstWorldToLevelBlock xs)
+firstWorldToLevelBlock (Block x y : xs) =  reverse((JBlock (x, y) 20.0 40.5 None): firstWorldToLevelBlock xs)
 firstWorldToLevelBlock (Cloud x y z :xs) = firstWorldToLevelBlock xs
 
 firstWorldToLevelCloud :: [Terrain] -> [Terrain]
