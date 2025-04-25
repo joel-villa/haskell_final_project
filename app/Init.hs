ba435bbd88725f6544ae170500da9089435b01f1
Joel Villarreal <joel.villa2329@gmail.com>
2025-04-24 20:01:15 -0600

    added 'active' Bool to Player Weapon

--- app/Init.hs
+++ app/Init.hs
@@ -37,7 +37,7 @@ starterSheep =
     inAir  = False,
     money  = 10, 
     sMoneyAndSValubles = [Potion {quantEffect=10,descriptor="Health Potion +10", effect=Healing}],
-    weapon = Weapon 5 "initial sword" 18 0 (15, -25),
+    weapon = Weapon 5 "initial sword" 18 0 (15, -25) False,
     facingRight = True
   }
 
