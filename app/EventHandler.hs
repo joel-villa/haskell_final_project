4d5a638fee767fbf4e8efa005cc5edc6632df267
Joel Villarreal <joel.villa2329@gmail.com>
2025-04-24 19:42:16 -0600

    added handleLeft and handleRight, updating the weapon hitBox now

--- app/EventHandler.hs
+++ app/EventHandler.hs
@@ -4,21 +4,34 @@ import Brillo.Interface.IO.Interact
 
 
 newHandleEvent :: Event ->World ->World 
-newHandleEvent(EventKey (Char 'd') Down _ _) w =w{hero= ((hero w){xVel=10})}
-newHandleEvent(EventKey (Char 'd') Up   _ _) w =w{hero= ((hero w){xVel=0})}
-newHandleEvent(EventKey (Char 'a') Down _ _) w =w{hero= ((hero w){xVel=(-5)})}
-newHandleEvent(EventKey (Char 'a') Up   _ _) w =w{hero= ((hero w){xVel=(0)})}
-newHandleEvent(EventKey (Char 'w') Down _ _) w =w{hero= (handleJump (hero w))}
--- newHandleEvent(EventKey (Char 'w') Up   _ _) w =w{hero= ((hero w){yVel=0})}
+newHandleEvent(EventKey (Char 'd') Down _ _) w = w{hero = (handleRight (hero w))}
+newHandleEvent(EventKey (Char 'd') Up   _ _) w = w{hero = ((hero w){xVel = 0})}
+newHandleEvent(EventKey (Char 'a') Down _ _) w = w{hero = (handleLeft (hero w))}
+newHandleEvent(EventKey (Char 'a') Up   _ _) w = w{hero = ((hero w){xVel = (0)})}
+newHandleEvent(EventKey (Char 'w') Down _ _) w = w{hero = (handleJump (hero w))}
 newHandleEvent(EventKey (SpecialKey KeySpace) Down _ _) w = w{hero = (useWeapon (hero w))}
 newHandleEvent _ w = w
 
+-- handle left user input, returning the new player
+handleLeft :: Player -> Player
+handleLeft p = p {xVel = -10, facingRight = False, weapon = newWeapon}
+  where
+    --updating weapon hit-box
+    newWeapon = (weapon p) {relativePos = (-15, -25)}
+
+-- handle right user input, returning the new player
+handleRight :: Player -> Player
+handleRight p = p {xVel = 10, facingRight = True, weapon = newWeapon}
+  where
+    --updating weapon hit-box
+    newWeapon = (weapon p) {relativePos = (15, -25)}
+-- handle jump user input, returning the new player
 handleJump :: Player ->Player
 handleJump plr 
   |inAir plr = plr 
   |otherwise = plr{yVel=20, inAir=True}
 
--- Handle use weapon input
+-- Handle use weapon input, returning the new player
 useWeapon :: Player -> Player
 useWeapon p = p {weapon = swungSword}
   where
