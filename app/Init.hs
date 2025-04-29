8131f2caa4543b8ca710b484789c701926776d89
nrunyan <nrunyan@unm.edu>
2025-04-29 12:21:54 -0600

    whooo watchout for that sheep

--- app/Init.hs
+++ app/Init.hs
@@ -39,9 +39,11 @@ starterSheep =
     sMoneyAndSValubles = [Potion {quantEffect=10,descriptor="Health Potion +10", effect=Healing}],
     weapon = Weapon 5 "initial sword" 18 0 (15, -25) False,
     facingRight = True,
-    hitBox= HitBox ((10),(-200)) ((20),(-200)) ((10), (-287)) (20, (-287))
+    hitBox= HitBox ((10),(-200)) ((20),(-200)) ((10), (-287)) (20, (-287)),
+    magic=Empty
   }
 
+--so you give it the actual x and this is what you do with it
 newHitBox:: Float-> Float-> Bool-> HitBox
 newHitBox x y False= HitBox (x-5,y+20) ((x+45),y+20) (x-5,(y-62)) ((x+45),(y-62))
 newHitBox x y True= HitBox (x-40,y+20) ((x+5),y+20) (x-40,(y-62)) ((x+5),(y-62))
@@ -50,6 +52,13 @@ updateHitboxB:: Float->Float->Bool->HitBox
 --           bottomLeft
 updateHitboxB x y False =HitBox ((x),(y+102)) ((x+100),(y+62)) ((x),y) ((x+100),y)
 updateHitboxB x y True =HitBox ((x-50),(y+102)) ((x+10),(y+62)) ((x-50),y) ((x+10),y)
+
+--so this needs the amount that you're moving it by
+generalUpdateHitBox:: Float->Float->HitBox->HitBox
+generalUpdateHitBox x y oldH= HitBox ((x1+x),(y1+y)) ((x2+x),(y1+y)) ((x1+x),(y2+y)) ((x2+x),(y2+y))
+  where
+    (x1,y1)= (topLt oldH)
+    (x2,y2)= (bottomRt oldH)
 
 
 
